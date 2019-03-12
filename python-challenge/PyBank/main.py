b4b9e022285bfb357b7b3ed8604520111c83c835
Amber <amberlukaesko@gmail.com>
2019-03-11 18:49:36 -0700

    completed pybank up through finding average of the changes in profit/losses

---
+++ python-challenge/PyBank/main.py
@@ -0,0 +1,73 @@
+import os
+import csv
+
+# csv_path = os.path.join("Users", "amberlukaesko", "Desktop", "UCI_DBC_ClassRepo", "UCIRV201902DATA3", "02-Homework", "03-Python", "Instructions", "PyBank", "Resources", "budget_data.csv")
+
+csv_path = "/Users/amberlukaesko/Desktop/UCI_DBC_ClassRepo/UCIRV201902DATA3/02-Homework/03-Python/Instructions/PyBank/Resources/budget_data.csv"
+
+
+with open(csv_path, newline="") as file:
+
+    budget_data_reader = csv.reader(file, delimiter=",")
+
+    csv_header = next(budget_data_reader)
+
+    row_count = 0
+    net_total = 0
+    for row in budget_data_reader:
+        row_count += 1
+        net_total += int(row[1])
+        row_val = int(row[1])
+
+with open(csv_path, newline="") as file:
+
+    budget_data_reader = csv.reader(file, delimiter=",")
+
+    csv_header = next(budget_data_reader)
+
+    next(budget_data_reader)
+
+    change = 0
+
+    for row in budget_data_reader:
+        next_row_val = int(row[1])
+        # if int(row[1]) > 0:
+        #     change += row_val - next_row_val
+        # else:
+        #     change += -(row_val + next_row_val)
+
+        # if row_val > 0 and next_row_val > 0:
+        #     change += row_val - next_row_val
+        # elif row_val > 0 and next_row_val < 0:
+        #     change += row_val + next_row_val
+        # elif row_val < 0 and next_row_val > 0:
+        #     change += row_val + next_row_val
+        # elif row_val < 0 and next_row_val < 0:
+        #     change += row_val + next_row_val
+
+        # if row_val > next_row_val:
+        #     change += -abs(row_val - next_row_val)
+        # else:
+        #     change += abs(row_val - next_row_val)
+
+
+        if row_val > next_row_val:
+            if row_val > 0 and next_row_val > 0:
+                change += next_row_val - row_val
+            elif row_val > 0 and next_row_val < 0:
+                change += -(row_val - next_row_val)
+            elif row_val < 0 and next_row_val < 0:
+                change += (abs(row_val) - abs(next_row_val))
+        else:
+            if row_val > 0 and next_row_val > 0:
+                change += next_row_val - row_val
+            elif row_val < 0 and next_row_val > 0:
+                change += abs(row_val) + abs(next_row_val)
+            elif row_val < 0 and next_row_val < 0:
+                change += abs(next_row_val) - abs(row_val)
+
+    average_change = change / row_count
+
+    print(row_count)
+    print(net_total)
+    print(average_change)
