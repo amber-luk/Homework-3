a9908a924c46c21c9b20bf1d3b20fd67ebde7b10
Amber <amberlukaesko@gmail.com>
2019-03-11 20:35:16 -0700

    added Resources folder with csv for each activity and almost completed PyPoll

---
+++ python-challenge/PyPoll/main.py
@@ -0,0 +1,60 @@
+import os
+import csv
+
+csv_path = os.path.join( "Resources", "election_data.csv")
+
+# csv_path = "/Users/amberlukaesko/Desktop/UCI_DBC_ClassRepo/UCIRV201902DATA3/02-Homework/03-Python/Instructions/PyPoll/Resources/election_data.csv"
+
+
+with open(csv_path, newline="") as file:
+
+    election_data_reader = csv.reader(file, delimiter=",")
+
+    csv_header = next(election_data_reader)
+
+    total_votes = 0
+
+    candidate_list = []
+
+    candidate_votes = []
+
+
+
+    for row in election_data_reader:
+        total_votes += 1
+        if row[2] not in candidate_list:
+            candidate_list.append(row[2])
+            candidate_index = candidate_list.index(row[2])
+            candidate_votes.append(1)
+        else:
+            candidate_index = candidate_list.index(row[2])
+            candidate_votes[candidate_index] += 1
+
+    percent_of_votes = []
+    for each in candidate_votes:
+        percent_of_votes.append(round(each*100 / total_votes, 2))
+
+
+
+
+    winner = candidate_votes.index(max(candidate_votes))
+    winning_candidate = candidate_list[winner]
+
+
+    print("Election Results")
+    print("-------------------------")
+    print(f"Total Votes: {total_votes}")
+    print("-------------------------")
+    for i in range(len(candidate_list)):
+        print(f"{candidate_list[i]}: {percent_of_votes[i]}% ({candidate_votes[i]})")
+    print("-------------------------")
+    print(f"Winner: {winning_candidate}")
+    print("-------------------------")
+
+
+results =  open("Election_Results.txt", 'w')
+results.write(f"""Election Results
+-------------------------
+Total Votes: {total_votes}
+-------------------------
+""")
